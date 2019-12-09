3a76fd38eab96a733c4ab96e99893a5a0ddb44c5
volkiza <volkiza>
2019-12-09 13:19:58 -0800

    Travis update

--- lab_3/test_suite.py
+++ lab_3/test_suite.py
@@ -7,8 +7,8 @@
 import unittest
 from testDistance import TestDistance
 from testStats import TestStats
-#from bootstrap_test import TestBoot
-#from boot_func_test import Boot_func_test
+from bootstrap_test import TestBoot
+from boot_func_test import Boot_func_test
 from z_score_test import TestZ_score
 from knn_test import TestKnn
 
@@ -21,8 +21,8 @@ def my_suite():
     result=unittest.TestResult()
     suite.addTest(unittest.makeSuite(TestDistance))
     suite.addTest(unittest.makeSuite(TestStats))
-    #suite.addTest(unittest.makeSuite(Boot_func_test))
-    #suite.addTest(unittest.makeSuite(TestBoot))
+    suite.addTest(unittest.makeSuite(Boot_func_test))
+    suite.addTest(unittest.makeSuite(TestBoot))
     suite.addTest(unittest.makeSuite(TestZ_score))
     suite.addTest(unittest.makeSuite(TestKnn))
     runner=unittest.TextTestRunner()
