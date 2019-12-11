7db9b0eb214f7e37ca141651bea87201592d3316
ShahAlvi <54693077+ShahAlvi@users.noreply.github.com>
2019-12-11 10:55:06 -0800

    Update stats.py

--- lab_3/statcalc/stats/stats.py
+++ lab_3/statcalc/stats/stats.py
@@ -1,35 +1,75 @@
-def s_mean(nums):
-    return float(sum(nums)) / max(len(nums), 1)
+class ListLenError(Exception):
+    pass
 
+
+class LargeSampleError(Exception):
+    pass
+
+
+def s_mean(nums):
+    try:
+        n = max(len(nums))
+        if n<2:
+            raise ListLenError()
+        return float(sum(nums)) / n, 1)
+    except ValueError:
+        print("Enter numerical values, wrong value entered")        
+    except ListLenError:
+        print("Enter more than one number")
+        
 
 def s_median(nums):
     n = len(nums)
+    try:
+        if n<2:
+            raise ListLenError()
+    except ListLenError:
+        print("Enter more than one number")        
     nums.sort()
-    if n % 2 == 0:
-        med1 = nums[n // 2]
-        med2 = nums[n // 2 - 1]
-        med = (med1 + med2) / 2
-    else:
-        med = nums[n // 2]
+    try:
+        if n % 2 == 0:
+            med1 = nums[n // 2]
+            med2 = nums[n // 2 - 1]
+            med = (med1 + med2) / 2
+        else:
+            med = nums[n // 2]
+    except ValueError:
+        print("Enter numerical values, wrong value entered")
     return med
 
 
 def s_var(nums):
     tot = 0
-    for i in nums:
-        tot += (s_mean(nums) - i) ** 2
+    try:
+        for i in nums:
+            tot += (s_mean(nums) - i) ** 2
+    except ValueError:
+        print("Enter numerical values, wrong value entered")
     variance = tot / len(nums)
     return variance
 
+
 def sample_var(nums):
     tot = 0
-    for i in nums:
-        tot += (s_mean(nums) - i) ** 2
-    variance = tot / (len(nums)-1)
+    try:
+        for i in nums:
+            tot += (s_mean(nums) - i) ** 2
+        n = (len(nums)-1) 
+    except ValueError:
+        print("Enter numerical values, wrong value entered")
+    try:
+        if n>5000:
+            raise LargeSampleError()
+    except LargeSampleError:
+        print("Enter less than 5001 observations to be considered a sample") 
+    
+    variance = tot / n
     return variance
+
 
 def s_std(nums):
     return s_var(nums)**0.5
+
 
 def sample_std(nums):
     return sample_var(nums)**0.5
